da6c8dc9c3bd96add6120891a0e39a276419541b
Nathan Papapietro <npapapietro95@gmail.com>
2022-07-07 12:56:29 -0500

    1 up 1 down with proper local exec

--- examples/complete/cluster.tf
+++ examples/complete/cluster.tf
@@ -18,9 +18,9 @@ module "example-complete" {
   source = "../../"
 
   cluster = {
-    install = true
+    install = false
     # Uncomment with `install = false` to destroy
-    # destroy    = true
+    destroy    = true
     version    = "1.22"
     subnet_ids = local.subnet_ids
     vpc_id     = module.vpc.vpc_id
