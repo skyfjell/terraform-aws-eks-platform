8ac4f6f9bd078f75a471df6f2e67b99df06f2382
Nathan Papapietro <npapapietro95@gmail.com>
2022-07-28 14:10:12 -0500

    More doc changes

--- variables.tf
+++ variables.tf
@@ -90,9 +90,10 @@ variable "config_flux" {
 
 variable "config_velero" {
   description = <<EOT
-    Configures velero and the velero bucket. An external velero bucket that is managed externally from this module can be 
-    passed in via `config_bucket = {id = "123"}`. If `config_bucket = {enable = true}` even with `install = false`
-    the bucket will remain created.
+    Configures velero and the velero bucket. An external velero bucket that 
+    is managed externally from this module can be passed in via 
+    `config_bucket = {id = "123"}`. If `config_bucket = {enable = true}` 
+    even with `install = false` the bucket will remain created.
   EOT
 
   type = object({
