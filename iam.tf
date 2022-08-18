cea8353b4274b0cbf44451ece6be895f551f7684
Nathan Papapietro <npapapietro95@gmail.com>
2022-08-18 11:23:41 -0500

    Fix // EBS CSI enable for k8s > 1.22 (#25)
    * Fix // Adding irsa for ebs
    
    * terraform-docs: automated action
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- iam.tf
+++ iam.tf
@@ -23,3 +23,19 @@ module "platform_edit_iam" {
   cluster_arn = module.cluster.cluster_arn
   attach      = local.cluster.install
 }
+
+
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = "ebs-csi-${local.labels.id}"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.cluster.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-node-sa", "kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
