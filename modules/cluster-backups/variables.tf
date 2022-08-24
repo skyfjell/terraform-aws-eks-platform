28bb43df2c4a3199f17bd835905767a626ed3dbf
Nathan Papapietro <npapapietro95@gmail.com>
2022-08-24 15:18:10 -0500

    Removing bad validation

--- modules/cluster-backups/variables.tf
+++ modules/cluster-backups/variables.tf
@@ -18,28 +18,6 @@ variable "config_bucket" {
       alias             = optional(string)
     }))
   })
-
-  # validation {
-  #   condition = anytrue([
-  #     // If enabled, with existing bucket and kms is used. The alias or kms id needs to be passed in
-  #     alltrue([
-  #       var.config_bucket.enable,
-  #       var.config_bucket.existing_id != null,
-  #       try(var.config_bucket.server_side_encryption_configuration.type, "aws:kms") != "AES256",
-  #       (
-  #         try(var.config_bucket.server_side_encryption_configuration.kms_master_key_id, null) != null
-  #         || try(var.config_bucket.server_side_encryption_configuration.alias, null) != null
-  #       )
-  #     ]),
-  #     // If enabled but not kms, ok
-  #     var.config_bucket.enable && try(var.config_bucket.server_side_encryption_configuration.type, "aws:kms") == "AES256",
-  #     // If enabled and exisiting bucket is null, default kms takes over
-  #     var.config_bucket.enable && var.config_bucket.existing_id == null,
-  #     // If not enabled
-  #     !var.config_bucket.enable
-  #   ])
-  #   error_message = "Existing bucket with aws:kms encryption needs a kms key or alias passed in."
-  # }
 }
 
 variable "velero_version" {
