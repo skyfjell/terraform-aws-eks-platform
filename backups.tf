f57149f07679a0702c5e0b5961951f0de67eed0f
Nathan Papapietro <npapapietro95@gmail.com>
2022-08-31 16:42:37 -0500

    Fix // Velero KMS policy

--- backups.tf
+++ backups.tf
@@ -7,6 +7,7 @@ locals {
   use_kms       = local.config_velero.enable && local.sse_type == "aws:kms"
   kms_alias     = try(local.config_velero.server_side_encryption_configuration.alias, null)
   kms_id        = try(local.config_velero.server_side_encryption_configuration.kms_master_key_id, null)
+  kms_key_id    = local.config_velero.enable && local.config_velero.existing_id == null ? one(module.velero_bucket.*.kms_arn) : try(coalesce(local.kms_alias), coalesce(local.kms_id), "")
 }
 
 
@@ -34,19 +35,3 @@ data "aws_kms_key" "kms" {
   key_id = local.config_velero.enable && local.config_velero.existing_id == null ? one(module.velero_bucket.*.kms_arn) : try(coalesce(local.kms_alias), coalesce(local.kms_id), "")
 }
 
-module "velero_iam" {
-  count   = local.config_velero.enable && length(local.config_velero.service_accounts.velero) > 0 ? 1 : 0
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.3.0"
-
-  role_name             = "${local.labels.id}-velero"
-  attach_velero_policy  = true
-  velero_s3_bucket_arns = data.aws_s3_bucket.velero.*.arn
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.cluster.oidc_provider_arn
-      namespace_service_accounts = local.config_velero.service_accounts.velero
-    }
-  }
-}
